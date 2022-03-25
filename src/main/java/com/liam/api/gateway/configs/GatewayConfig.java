40cab75500cbf771ef43695df886e322941acd90
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-25 11:39:02 -0500

    adding RouteLocator instead of application configurations

--- src/main/java/com/liam/api/gateway/configs/GatewayConfig.java
+++ src/main/java/com/liam/api/gateway/configs/GatewayConfig.java
@@ -2,20 +2,22 @@ package com.liam.api.gateway.configs;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
 	
+	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
 		
 		return routeLocatorBuilder.routes()
-				.route(p -> p.path("/employees")
-						.uri("http://localhost:9005"))
-				.route(p -> p.path("/companies")
-						.uri("http://localhost:9006"))
-				.route(p -> p.path("/departments")
-						.uri("http://localhost:9001"))
+				.route(p -> p.path("/employees/**")
+						.uri("lb://EMPLOYEE-SERVICE"))
+				.route(p -> p.path("/companies/**")
+						.uri("lb://COMPANY-SERVICE"))
+				.route(p -> p.path("/departments/**")
+						.uri("lb://DEPARTMENT-SERVICE"))
 				.build();
 	}
 
