78f608e917ec973b91cc2aa77ad352515b4588b2
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-25 12:48:09 -0500

    adding resilience4j circuitbreaker

--- src/main/java/com/liam/api/gateway/configs/GatewayConfig.java
+++ src/main/java/com/liam/api/gateway/configs/GatewayConfig.java
@@ -1,9 +1,17 @@
 package com.liam.api.gateway.configs;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class GatewayConfig {
@@ -12,13 +20,23 @@ public class GatewayConfig {
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
 		
 		return routeLocatorBuilder.routes()
-				.route(p -> p.path("/employees/**")
+				.route(p -> p
+						.path("/employees/**")
 						.uri("lb://EMPLOYEE-SERVICE"))
-				.route(p -> p.path("/companies/**")
+				.route(p -> p
+						.path("/companies/**")
 						.uri("lb://COMPANY-SERVICE"))
-				.route(p -> p.path("/departments/**")
+				.route(p -> p
+						.path("/departments/**")
+						.filters(f -> f.circuitBreaker(c -> c.setName("circuitBreaker").setFallbackUri("/departmentFallback")))
 						.uri("lb://DEPARTMENT-SERVICE"))
 				.build();
+	}
+	
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()).timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build()).build());
 	}
 
 }
